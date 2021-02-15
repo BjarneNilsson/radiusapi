db3be663e888ec9a75ba2683ac950d52de3dbbe3
bn-1980 bn-1980 <bjarne-priv@holmedal.net>
2021-02-15 07:57:47 +0100

    Moced connectiion string to Parameters,json added function to read string from config and fixed a few bugs user without vlan now handeled correctly

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -2,6 +2,9 @@
 using Microsoft.Extensions.Logging;
 using net.holmedal;
 using Npgsql;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
 
 namespace api.Controllers
 {
@@ -9,8 +12,13 @@ namespace api.Controllers
   [Route("")]
   public class WeatherForecastController : ControllerBase
   {
-   
-    [HttpGet("GetUser")]
+
+        static string getDbConnection() {
+            var Path1 = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+             var Builder = new ConfigurationBuilder().SetBasePath(Path1).AddJsonFile("Parameters.json", optional: true, reloadOnChange: true);
+             return  Builder.Build().GetSection("Db").GetSection("ConnetionString").Value;
+        }
+        [HttpGet("GetUser")]
     [Produces("application/json", new string[] {})]
     public User JsonResult(string usr)
     {
@@ -20,7 +28,7 @@ namespace api.Controllers
         Password = (string) null,
         Vlan = new int?()
       };
-      NpgsqlConnection connection = new NpgsqlConnection("Host=10.47.1.235;Username=radius;Password=radauth;Database=radius");
+      NpgsqlConnection connection = new NpgsqlConnection(getDbConnection());
       connection.Open();
       NpgsqlCommand npgsqlCommand = new NpgsqlCommand("SELECT value from radcheck where attribute='Cleartext-Password' and username like @sr  ", connection);
       npgsqlCommand.Parameters.AddWithValue("@sr", (object) usr);
@@ -32,10 +40,12 @@ namespace api.Controllers
       npgsqlCommand.CommandText = str1;
       npgsqlCommand.Parameters.AddWithValue("dr", (object) usr);
       NpgsqlDataReader npgsqlDataReader2 = npgsqlCommand.ExecuteReader();
-      while (npgsqlDataReader2.Read())
-      {
-        string str2 = npgsqlDataReader2.GetString(0);
-        user.Vlan = new int?(int.Parse(str2.Remove(0, 4)));
+            while (npgsqlDataReader2.Read()) { 
+                   
+                    string str2 = npgsqlDataReader2.GetString(0);
+                if (str2.Remove(0, 4).Length > 0)
+                    user.Vlan = new int?(int.Parse(str2.Remove(0, 4)));
+                else user.Vlan = null;
       }
       npgsqlDataReader2.Close();
       npgsqlDataReader2.DisposeAsync();
@@ -48,23 +58,26 @@ namespace api.Controllers
     [HttpPost("AddUser")]
     public void Js([FromBody] User u)
     {
-      NpgsqlConnection connection = new NpgsqlConnection("Host=10.47.1.235;Username=radius;Password=radauth;Database=radius");
+      NpgsqlConnection connection = new NpgsqlConnection(getDbConnection());
       connection.Open();
       NpgsqlCommand npgsqlCommand = new NpgsqlCommand("insert into radcheck(username,attribute,op,value) values(@n,'Cleartext-Password',':=', @p );", connection);
       npgsqlCommand.Parameters.AddWithValue("n", (object) u.Name);
       npgsqlCommand.Parameters.AddWithValue("p", (object) u.Password);
       npgsqlCommand.ExecuteNonQuery();
-      string str = "insert into  radusergroup (username,groupname,priority) values(@u,@g,0);";
-      npgsqlCommand.CommandText = str;
-      npgsqlCommand.Parameters.AddWithValue("@u", (object) u.Name);
-      npgsqlCommand.Parameters.AddWithValue("g", (object) ("vlan" + u.Vlan.ToString()));
-      npgsqlCommand.ExecuteNonQuery();
+            if (u.Vlan != null)
+            {
+                string str = "insert into  radusergroup (username,groupname,priority) values(@u,@g,0);";
+                npgsqlCommand.CommandText = str;
+                npgsqlCommand.Parameters.AddWithValue("@u", (object)u.Name);
+                npgsqlCommand.Parameters.AddWithValue("g", (object)("vlan" + u.Vlan.ToString()));
+                npgsqlCommand.ExecuteNonQuery();
+            }
     }
 
     [HttpPut("UpdateUser")]
     public void UpdateUser([FromBody] User U)
     {
-      NpgsqlConnection connection = new NpgsqlConnection("Host=10.47.1.235;Username=radius;Password=radauth;Database=radius");
+      NpgsqlConnection connection = new NpgsqlConnection(getDbConnection());
       connection.Open();
       NpgsqlCommand npgsqlCommand = new NpgsqlCommand("update radcheck set value=@p where username like @u and attribute like 'Cleartext-Password';", connection);
       npgsqlCommand.Parameters.AddWithValue("u", (object) U.Name);
